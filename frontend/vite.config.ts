a81c4fa7606959362c854c48008ed284fb6d2191
Biraj Mishra <birajmishra@hotmail.com>
2026-03-01 23:54:23 +0800

    light mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+//import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -17,7 +17,8 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), basicSsl(), mode === "development" && componentTagger()].filter(Boolean),
+  //plugins: [react(), basicSsl(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
